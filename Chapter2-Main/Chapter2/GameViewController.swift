9a1a93729aa49562b53d02380b6819a20036da00
Christopher Bell <christopher@christophers-macbook.local>
2014-12-12 04:22:43 +0000

    test

---
+++ Chapter2-Main/Chapter2/GameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GameViewController.swift
+//  Chapter2
+//
+//  Created by Christopher on 12/12/2014.
+//  Copyright (c) 2014 Christopher Bell. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scnView = self.view as SCNView
+        let scene = BoxScene()
+        scnView.scene = scene
+        
+        scnView.backgroundColor = UIColor.whiteColor()
+        
+        scnView.allowsCameraControl = true
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
